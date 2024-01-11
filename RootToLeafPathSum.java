6d5bb316a816d6a0676c693522161e9b901547a3
hariharanalagarsamy <hariharan.alagarsamy1@gmail.com>
2024-01-11 13:50:05 +0530

    Create RootToLeafPathSum.java
    https://leetcode.com/problems/path-sum/?envType=study-plan-v2&envId=top-interview-150

---
+++ RootToLeafPathSum.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+//https://leetcode.com/problems/path-sum/?envType=study-plan-v2&envId=top-interview-150
+class RootToLeafPathSum {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+       if(root == null){
+            return false;
+        }
+        if( root.left == null && root.right == null){
+            return targetSum - root.val == 0;
+        }
+        
+        return hasPathSum(root.left,targetSum - root.val) ||
+        hasPathSum(root.right,targetSum - root.val);
+    }
+
+}
